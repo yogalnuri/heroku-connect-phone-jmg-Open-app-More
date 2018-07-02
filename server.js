09e763a54ca6a7936b7629c73ac74e9848df4c3d
CTS\675673 <yogalnuri@gmail.com>
2018-07-02 12:06:21 +0200

    no message

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.post('/update', function(req, res) {
     var newContact = req.body;
     console.log('Name Param-->>'+req.body.name);
     console.log('Base Url-->'+req.baseUrl);
+    console.log('Request Body-->'+req.body);
     if (!req.body.name) {
         handleError(res, "Invalid user input", "Must provide a name.", 400);
     }
