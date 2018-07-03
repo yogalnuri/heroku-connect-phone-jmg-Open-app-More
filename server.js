6ea5d34de1c747f0861be5181cca0894b4efc2f3
CTS\675673 <yogalnuri@gmail.com>
2018-07-03 15:54:45 +0200

    no message

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.post('/update', function(req, res) {
             // watch for any connect issues
             conn.query('select Account__c, Name, Id from salesforce.Maquina__c where Name=$1', [req.body.maquina], function(err, results){
                     if(err===null || err===undefined){
-                        res.json({"records":results.rows[0].account__c, "origen":req.body.maquina})
                         conn.query("Insert into salesforce.Case (Account, Maquina__c, Status, Origin, Subject) Values($1,$2,$3,$4)",
                                     [results.rows[0].account__c,results.rows[0].Id,req.body.status,req.body.origin,req.body.subject ],
                                     function(error, resultado){
