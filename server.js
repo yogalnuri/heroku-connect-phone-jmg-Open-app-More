75553a224eeb8b3431dfe7d046bd93c75bd92b35
CTS\675673 <yogalnuri@gmail.com>
2018-07-03 12:07:56 +0200

    no message

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/update', function(req, res) {
             // watch for any connect issues
             conn.query('select Account__c, Name from salesforce.Maquina__c where Name=$1', [req.body.maquina], function(err, results){
                     if(err===null || err===undefined){
-                        res.json({"records":results.records.rows[0], "origen":req.body.maquina})
+                        res.json({"records":results.records.rows, "origen":req.body.maquina})
                     }else{
                         handleError(res, "Error query", err.message, 400);
                     }
