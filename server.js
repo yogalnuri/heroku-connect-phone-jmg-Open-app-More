23989ebaf1eea6cae277afe9bd4c05dcef08fd20
ACERPORTATIL\yogal <yogalnuri@gmail.com>
2018-07-07 14:27:55 +0200

    no message

--- server.js
+++ server.js
@@ -51,25 +51,25 @@ app.post('/update', function(req, res) {
 app.get('/update', function(req, res) {
 	console.log(req.query);
 
-    if (!req.body.maquina) {
+    if (!req.query.maquina) {
         handleError(res, "Invalid user input", "Must provide a  machine name.", 400);
-    }else if (!req.body.status) {
+    }else if (!req.query.status) {
         handleError(res, "Invalid user input", "Must provide a Status.", 400);
-    } if (!req.body.origin) {
+    } if (!req.query.origin) {
         handleError(res, "Invalid user input", "Must provide an Origin.", 400);
-    } if (!req.body.subject) {
+    } if (!req.query.subject) {
         handleError(res, "Invalid user input", "Must provide an Subject.", 400);
     }else{
         pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
             // watch for any connect issues
-            conn.query('select Account__c, Name, Id from salesforce.Maquina__c where Name=$1', [req.body.maquina], function(err, results){
+            conn.query('select Account__c, Name, Id from salesforce.Maquina__c where Name=$1', [req.query.maquina], function(err, results){
                 if(err===null || err===undefined){
                     conn.query('select Max(External_Case_Id__c) as External_Case_Id__c from salesforce.Case', function(errorCase, resultsCase){
                         if(errorCase===null || errorCase===undefined){
                             var externalId= resultsCase.rows[0].external_case_id__c+1;
                             console.log("Hago el insert");
                             conn.query("Insert into salesforce.Case (AccountId, Maquina_Averiada__c, Status, Origin, Subject, External_Case_Id__c) Values($1,$2,$3,$4,$5,$6)",
-                                        [results.rows[0].account__c, results.rows[0].Id, req.body.status, req.body.origin, req.body.subject, externalId ],
+                                        [results.rows[0].account__c, results.rows[0].Id, req.query.status, req.query.origin, req.query.subject, externalId ],
                                         function(error, resultado){
                                             done();
                                             if(error===null || error===undefined){
